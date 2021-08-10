0540c9db290435454dba7f9a3cad5ae5a9de74a5
ravindu01manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-10 15:40:39 +0530

    👑KingAndQueen❤Sew❤Ravindu👑ⁿᵉʷ ᵘᵖᵈᵃᵗᵉ

---
+++ sewqueen/StringSession.js
@@ -0,0 +1,38 @@
+/* Codded by @Ravindu Manoj
+
+Telegram: t.me/RavinduManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Ravindu Manoj
+*/
+
+const fs = require('fs');
+
+class StringSession {
+    constructor() {
+    }
+
+    deCrypt(string = undefined) {
+        if ('QUEENSEW_SESSION' in process.env && string === undefined) {
+            string = process.env.STRING_SESSION;
+        } else if (string !== undefined) {
+            if (fs.existsSync(string)) {
+                string = fs.readFileSync(string, {encoding:'utf8', flag:'r'});
+            }
+        }
+        
+        var split = string.split(';;;');
+        if (split.length >= 2) {
+            return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
+        }
+    }
+
+    createStringSession(dict) {
+        return 'QUEENSEW;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+    }
+}
+
+module.exports = StringSession;
