e7a99becb4ed54b1c657e450c3c72abf3868b271
ravindu01manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-10 22:55:19 +0530

    👑KingAndQueen❤Sew❤Ravindu👑ⁿᵉʷ ᵘᵖᵈᵃᵗᵉ

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 Whats bot - Ravindu Manoj
+t.me/RavinduManoj
 */
 
 const fs = require("fs");
