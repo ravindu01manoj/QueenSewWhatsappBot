5d60193faf8e5b5ec588d498f9504628ee031ceb
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-12 06:20:41 +0530

    👑KingAndQueen❤Sew❤Ravindu👑

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -507,7 +507,7 @@ if (config.WORKTYPE == 'private') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: Config.THUM, quoted: message.data});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: config.THUM, quoted: message.data});
         });
     }));
 
@@ -974,7 +974,7 @@ else if (config.WORKTYPE == 'public') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: Config.THUM, quoted: message.data});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: config.THUM, quoted: message.data});
         });
     }));
 
@@ -1169,7 +1169,7 @@ else if (config.WORKTYPE == 'public') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: Config.THUM, quoted: message.data});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4,thumbnail: config.THUM, quoted: message.data});
         });
     }));
 
