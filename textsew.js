a88ef08ceae7a3f872933c12e1540fd286692d52
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-20 22:46:19 +0530

    Upgrade 1.2.1 - Full Control

---
+++ textsew.js
@@ -0,0 +1,26 @@
+const Raviya = require('./config');
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./textsew/' + Raviya.TEXT + '.json')) {
+    console.log(
+        chalk.green.bold('Loading ' + Raviya.TEXT + ' textsew...')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./textsew/' + Raviya.TEXT + '.json'));
+} else {
+    console.log(
+        chalk.red.bold('')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./textsew/SEW.json'));
+}
+
+function getrule(file) {
+    return json['RULE'][file];
+}
+
+module.exports = {
+    textsew: json,
+    getrule: getrule
+}
