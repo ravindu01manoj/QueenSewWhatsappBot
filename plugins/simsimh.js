c5a6986334686a17a5594a898bc98b86e4ab2788
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-22 05:15:55 +0530

    Upgraded (1.2.2 - Full Control) - SEW QUEEN

--- plugins/simsimh.js
+++ plugins/simsimh.js
@@ -3,6 +3,7 @@
 Telegram: t.me/RavinduManoj
 Facebook: https://www.facebook.com/ravindu.manoj.79
 
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
