3210c20ce0f806c7c05c6fecbfca99d8730e589f
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-30 21:15:03 +0530

    ❁SEW QUEEN ⚓ 2.0.0 ⚛ FULL CONTROL❁
    *♕❄SEW QUEEN 2.0.0 - Full Control - Dual Bot Mode❄♔*
    
    1st bot
    *✨Sew Queen Whatsapp Bot ✨★➳*
    
    
    ❯❯❯added new ttp list ==> .sewttp
    
    ❯❯❯new unit convart system ==> .unit 1 kg g / .bitunit 1 Gb Mb
    
    ❯❯❯site to pdf ==> .sitepdf
    
    ❯❯❯inbox block system ==> for activate  .setvar INBO_BLOCK:true  for disable  .setvar INBO_BLOCK:false
    
    ❯❯❯anime image and gif download 100000+ ==> .anime pic / .anime gif
    
    ❯❯❯carbon image make upgaded ==> .carbon
    
    ❯❯❯adding voice massage with Fake Recording option
    
    ❯❯❯fixed some bot bugs and antispam system
    
    ❯❯❯try to fix facebook download error ==> .fb
    
    ❯❯❯adding mediafire Download ==> .mapk / .mzip
    
    ❯❯❯spotify download ==> .spotyfi 
    
    ❯❯❯new text maker list ==> .sew3maker (adding 100+ new text to image soonly)
    
    ❯❯❯dual bot mod ==> .kingraviya && .queensew or .setvar BOT_MODE:kingraviya && .setvar BOT_MODE:queensew
    
    
    2nd Bot
    *✨King Raviya 18 + whatsapp bot ✨★➳*
    
    
    ❯❯❯phub search ==> .pornhub your text
    
    ❯❯❯phub search list download ==> .xxx link
    
    ❯❯❯xnxx download  ==> .xnxx link 
    
    ❯❯❯porn pic ==> check the .18plus
    
    ❯❯❯porn gif ==> check the .18plus
    
    && Fixed some Errors And Bugs
    
    
    
    *SEW QUEEN 2.0.0  - FULL CONTROL - ද්විත්ව බොට් ප්‍රකාරය♔*
    
    1 වන බොට්
    *Sew Queen Whatsapp Bot★ ➳*
    
    
    ❯❯❯නව ttp ලැයිස්තුව එකතු කරන ලදි ==> .sewttp
    
    ❯❯❯නව ඒකක පරිවර්තන ගැන්වීමේ පද්ධතිය ==> .unit 1 kg  g / .bitunit 1 Gb Mb
    
    ❯❯❯වෙබ් අඩවිය pdf බවට ==> .sitepdf
    
    ❯❯❯ඉන්බොක්ස් පැමිනෙන අය බ්ලොක් කිරීමේ පද්දතිය ==> සක්‍රිය කිරීම සඳහා .setvar INBO_BLOCK:true අක්‍රීය කිරීම සඳහා setvar INBO_BLOCK:false
    
    ❯❯❯ඇනිමෙ ප්‍රතිරූපය සහ ගිෆ් බාගැනීම් 100000+ ==> .anime pic / .anime gif
    
    ❯❯❯carbon image ඉහළ නංවා ඇත ==> .carbon
    
    ❯❯❯ව්‍යාජ පටිගත කිරීමේ විකල්පය සමඟ voice massage  එකතු කිරීම
    
    ❯❯❯සමහර බොට් bug  සහ ඇන්ටිස්පෑම් පද්ධතිය upgrade කර ඇත
    
    ❯❯❯ෆේස්බුක් බාගැනීමේ දෝෂය නිවැරදි කිරීමට උත්සාහ කරන්න ==> .fb
    
    ❯❯❯මීඩියාෆයර් ලින්ක් ඩදුන්ලෝඩ් එකතු කිරීම ==> .mapk / .mzip
    
    ❯❯❯spotify බාගැනීම ==> .spotyfi
    
    ❯❯❯නව ලෝගො මේකර් ලැයිස්තුව ==> .sew3maker (ඉක්මනින් පින්තූරයට නව පෙළ 100+ එකතු කිරීම)
    
    ❯❯❯ද්විත්ව බොට් මොඩ් (බොට්ව මාරු කිරීම සදහා යොදා ගන්න ) ==> .kingraviya && .queensew
    
    
    2 වෙනි බොට් 
    *King Raviya 18 + වට්ස්ඇප් බොට් ★ ➳*
    
    
    ❯❯❯pornhub සෙවුම ==> .pornhub your text
    
    ❯❯❯ඉහත සෙවුම් ලැයිස්තුව බාගන්න ==> .xxx link
    
    ❯❯❯xnxx බාගැනීම ==> .xnxx link
    
    ❯❯❯කාමුක පින්තූර ==> .18plus පරීක්‍ෂා කරන්න
    
    ❯❯❯කාමුක gif ==> .18plus පරීක්‍ෂා කරන්න
    
    && සමහර දෝෂ නිරාකරණය කර ඇත

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,1 +1,56 @@
+const QueenSew = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const Raviya = require('../config');
+var r_text = new Array ();
+r_text[1] = Raviya.RGMSA
+r_text[2] = Raviya.RGMSB
+r_text[3] = Raviya.RGMSC
+r_text[4] = Raviya.RGMSD
+r_text[5] = Raviya.RGMSE
 
+var i = Math.floor(5*Math.random())
+
+if (Raviya.PSW !== 'kingraviya') {
+        
+QueenSew.newcmdaddtosew({ pattern: 'tiktok ?(.*)', fromMe: true, disc: 'tiktok video download without watermark'}, async (message, match) => {
+        const sewtik = match[1]
+        if (!sewtik) return await message.client.sendMessage(message.jid,'need tiktok link',MessageType.text);
+         await message.client.sendMessage(message.jid,'downloading your video',MessageType.text)
+        await axios
+          .get(`https://zenzapi.xyz/api/downloader/tiktok?url=${sewtik}&apikey=${r_text[i]}`)
+          .then(async (response) => {
+            const {
+              nowatermark,
+            } = response.data.result
+    
+            const videoBuffer = await axios.get(nowatermark, {responseType: 'arraybuffer'})
+    
+            await message.client.sendMessage(message.jid,'uploading your video',MessageType.text, {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,'cant find 🥴🥴🥴',MessageType.text, {quoted: message.data}),
+        )});
+
+QueenSew.newcmdaddtosew({ pattern: 'tiktok ?(.*)', fromMe: false, disc: 'tiktok video download without watermark'}, async (message, match) => {
+        const sewtik = match[1]
+        if (!sewtik) return await message.client.sendMessage(message.jid,'need tiktok video link',MessageType.text);
+         await message.client.sendMessage(message.jid,'downloading your video',MessageType.text)
+        await axios
+          .get(`https://zenzapi.xyz/api/downloader/tiktok?url=${sewtik}&apikey=${r_text[i]}`)
+          .then(async (response) => {
+            const {
+              nowatermark,
+            } = response.data.result
+    
+            const videoBuffer = await axios.get(nowatermark, {responseType: 'arraybuffer'})
+    
+            await message.client.sendMessage(message.jid,'uploading your video',MessageType.text, {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,'cant find 🥴🥴🥴',MessageType.text, {quoted: message.data}),
+        )});
+}
