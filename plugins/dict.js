a8a291831402f451186398e27957cd296337ed74
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-08-30 15:15:24 +0530

    ❁SEW QUEEN ⚓ 2.0.0 ⚛ FULL CONTROL❁

--- plugins/dict.js
+++ plugins/dict.js
@@ -6,6 +6,7 @@ Facebook: https://www.facebook.com/ravindu.manoj.79
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
+
 Whats bot - Ravindu Manoj
 */
 
