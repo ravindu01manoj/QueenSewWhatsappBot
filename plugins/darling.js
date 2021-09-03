f1bf8cd6b90320ef083c71a62a50fff597a10b17
Ravindu Manoj <88134633+ravindu01manoj@users.noreply.github.com>
2021-09-04 02:49:33 +0530

    ✧✧Bug Fixed✧✧

--- plugins/darling.js
+++ plugins/darling.js
@@ -16,20 +16,20 @@ const Config = require('../config')
  if (Config.PSW !== 'kingraviya') {
 QueenSew.newcmdaddtosew({pattern: 'msglv', fromMe: false, desc: SEW }, (async (message, match) => {
 var sewrm = new Array ();
-sewrm [0] = zzaaa
-sewrm [1] = zzaab
-sewrm [2] = zzaac
-sewrm [3] = zzaad
-sewrm [4] = zzaae
-sewrm [5] = zzaaf
-sewrm [6] = zzaag
-sewrm [7] = zzaah
-sewrm [8] = zzaai
-sewrm [9] = zzaaj
-sewrm [10] = zzaak
-sewrm [11] = zzaal
-sewrm [12] = zzaam
+sewrm [0] = "zzaaa"
+sewrm [1] = "zzaab"
+sewrm [2] = "zzaac"
+sewrm [3] = "zzaad"
+sewrm [4] = "zzaae"
+sewrm [5] = "zzaaf"
+sewrm [6] = "zzaag"
+sewrm [7] = "zzaah"
+sewrm [8] = "zzaai"
+sewrm [9] = "zzaaj"
+sewrm [10] = "zzaak"
+sewrm [11] = "zzaal"
+sewrm [12] = "zzaam"
 var s = Math.floor(13*Math.random())
-await message.client.sendMessage(message.jid, fs.readFileSync('./VoiceClip/' + sewrm[s] + '.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data, ptt: false})
+await message.client.sendMessage(message.jid, fs.readFileSync('./VoiceClip/' + '${sewrm[s]}' + '.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, ptt: false})
 }));
 }
